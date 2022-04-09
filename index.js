cd6fa3c2f90d328ce7cd22a97a88a5477cd6c620
Macko++ <max.konietzko.maya@gmail.com>
2022-04-09 11:21:09 -0500

    feat: :sparkles: Made the slider change the title size
    Made the range slider change the title size and put everything in the true middle of the screen and made everything size responsive
    
    The slider wasnt changing the title size, nothing was in the middle

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-i
+// document.querySelector('.button').addEventListener('click', function() {
+// 	document.querySelector('.title').innerHTML = document.querySelector('.range').value;
+// });
+
+$(() => {
+
+	
+	$('.button').on("click", () => {
+		var rangeVal = $(".range").val();
+		console.log(rangeVal);
+		$(".title").html(`<h1 class="title is-${8 - rangeVal}">Macko++</h1>`);
+	});
+});
