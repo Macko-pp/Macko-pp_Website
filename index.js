1ce7dd841957d38f7eb6e1d423048deefdf9824a
Macko++ <max.konietzko.maya@gmail.com>
2022-05-16 10:44:39 -0500

    fix: :sparkles: made the navbar buttons work
    made the button on the navbar lead to my different projects and my email

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ $(() => {
 	$('.button').on("click", () => {
 		var rangeVal = $(".range").val();
 		console.log(rangeVal);
-		$(".title").html(`<h1 class="title is-${8 - rangeVal}">Macko++</h1>`);
+		$(".main-title").html(`<h1 class="title main-title is-${8 - rangeVal}">Macko++</h1>`);
 	});
 	
 });
